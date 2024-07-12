7219b5edec101831ee4e0ca037b8ebc5ee9fa66b
Rodney Giles <rodney@rodneygiles.net>
2024-07-12 16:26:29 -0500

    better cli

---
+++ scripts/runcore-up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# display help
+function help() {
+  case $1 in
+    up)
+      echo "start a local dev environment"
+      ;;
+    *)
+      echo "  $RUNCORE up"
+      ;;
+  esac
+}
+
+# run something
+case $1 in
+  help) help ${@:2};;
+  up) up ${@:2};;
+esac
